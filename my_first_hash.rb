21b7872ee935060d676eb088963a6bd01f43568a
Theodore Schafer <theodore.schafer@gmail.com>
2020-08-27 14:29:55 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+my_hash = {"cool_stuff" => "dark souls"}
   
 end
 
@@ -39,5 +39,5 @@ def adding
 
 
   # return the shipping_manifest hash below
-
+  shipping_manifest
 end
